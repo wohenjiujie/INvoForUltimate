4d7105fa0998e837f08395d080358557adbcde9d
INvo <xiaomu1079@outlook.com>
2019-11-12 23:21:10 +0800

    TrackHistoryActivity中onGetID的JSONObject解析需要完善

---
+++ app/src/main/java/com/graduationproject/invoforultimate/constant/TrackApplication.java
@@ -0,0 +1,19 @@
+package com.graduationproject.invoforultimate.constant;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Created by INvo
+ * on 2019-11-05.
+ */
+public class TrackApplication extends Application {
+    private static Context context;
+    public void onCreate() {
+        super.onCreate();
+        context = getApplicationContext();
+    }
+    public static Context getContext(){
+        return context;
+    }
+}
---
+++ app/src/main/java/com/graduationproject/invoforultimate/service/TrackHistoryService.java
@@ -0,0 +1,71 @@
+package com.graduationproject.invoforultimate.service;
+
+import com.graduationproject.invoforultimate.constant.TrackApplication;
+import com.graduationproject.invoforultimate.constant.TrackInfo;
+import com.graduationproject.invoforultimate.initialize.InitializeTerminal;
+
+import java.io.IOException;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+/**
+ * Created by INvo
+ * on 2019-11-05.
+ */
+ public  class TrackHistoryService {
+
+    private OkHttpClient okHttpClient = new OkHttpClient();
+    private Response response;
+    private RequestBody requestBody = null;
+    private MediaType mediaType;
+    private Request request;
+    private TrackInfo trackInfo;
+    private String content;
+    private InitializeTerminal initializeTerminal;
+    private int counts;
+
+    public int getTrackCounts(){
+
+        return trackCounts();
+//        return getCounts();
+    }
+
+    private int trackCounts(){
+        initializeTerminal = new InitializeTerminal();
+        final int[] result = new int[1];
+        Thread thread = new Thread(() -> {
+            content = "http://xiaomu1079.club/searchCounts/" +initializeTerminal.getTerminal(TrackApplication.getContext());
+            request = new Request.Builder().url(content).get().build();
+            try {
+                response = okHttpClient.newCall(request).execute();
+                result[0] =Integer.parseInt(response.body().string());
+//                setCounts(result[0]);
+//                Log.d("TrackHistoryService", a);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        thread.start();
+        /*try {
+            thread.join();
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }*/
+//        long a = initializeTerminal.getTerminal(TrackApplication.getContext());
+//        Log.d("TrackHistoryService", "a:" + a);
+        return result[0];
+    }
+
+    private void setCounts(int result) {
+        this.counts = result;
+    }
+
+
+    private int getCounts(){
+        return this.counts;
+    }
+}
